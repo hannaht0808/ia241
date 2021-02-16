a210d9bb25b767baafe30cc0d1145aadc6b9dfef
EC2 Default User <ec2-user@ip-172-31-47-30.ec2.internal>
2021-02-16 15:49:50 +0000

    lec5

---
+++ lec5.py
@@ -0,0 +1,80 @@
+"""
+lec5 if statment 
+"""
+
+#import this 
+
+#print(  1
+      #      + 2)
+            
+#print(2,3,4,5,6,7)
+
+
+# = 1\
+ #   +1
+#print(m)
+
+
+#a = [1,2,3]
+
+#b = [1,2,3]
+
+#print(id([1,2,3]))
+#print(id(a))
+#print(id(b))
+
+
+
+
+#X = None
+
+#print(id(X))
+#print(id(None))
+
+#print(X == None)
+#print(X is None)
+
+#y = []
+#print(y == None)
+#print(y is None)
+
+#print(True and False)
+
+
+
+#if 2>1:
+ #   print('2<1')
+    
+#if 2<=1:
+ #   print('2<=1')
+
+#print('not in the id block')
+
+
+
+#if 2<=1:
+ #   print('2>1')
+#else:
+ #   print('2>1')
+    
+#if 2<=2:
+ #   print('2<=2')
+#else:
+ #   print('2>2')
+ 
+#if 2<=1:
+   # print('2<=1')
+#elif 2<=2:
+  #  print('2<=2')
+#else:
+  #print('2>1')
+  
+
+if None:
+      print(1)
+elif {}:
+    print(2)
+elif '0':
+    print(3)
+else:
+    print(4)
